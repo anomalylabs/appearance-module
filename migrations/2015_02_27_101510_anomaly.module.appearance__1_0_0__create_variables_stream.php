a6c8852de57039a3a19026a799762e38775248cb
Ryan Thompson <service@aiwebsystems.com>
2015-07-02 23:30:23 -0500

    Naming

--- migrations/2015_02_27_101510_anomaly.module.appearance__1_0_0__create_variables_stream.php
+++ migrations/2015_02_27_101510_anomaly.module.appearance__1_0_0__create_variables_stream.php
@@ -3,13 +3,13 @@
 use Anomaly\Streams\Platform\Database\Migration\Migration;
 
 /**
- * Class AnomalyModuleVariablesCreateVariablesStream
+ * Class AnomalyModuleAppearanceCreateVariablesStream
  *
  * @link          http://anomaly.is/streams-platform
  * @author        AnomalyLabs, Inc. <hello@anomaly.is>
  * @author        Ryan Thompson <ryan@anomaly.is>
  */
-class AnomalyModuleVariables_1_0_0_CreateVariablesStream extends Migration
+class AnomalyModuleAppearance_1_0_0_CreateVariablesStream extends Migration
 {
 
     /**
